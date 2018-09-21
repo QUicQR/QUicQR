1f7a63424d7b37c38b490c9fe4e03d9aa8dfd73b
Justin Grierson <ju3tin@hotmail.co.uk>
2018-09-21 12:41:38 +0100

    asdasd

--- test.php
+++ test.php
@@ -128,7 +128,10 @@ $conn->close();
 <html>
 <head>
 
+    <link href="bootstrap.min.css" rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
 
 </head>
 <body>
@@ -141,5 +144,35 @@ $conn->close();
   <input  name="password"  type="text">
   <input  type="submit"  name="submit"  value="submit" >
 </form>
+
+
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="jquery-slim.min.js"><\/script>')</script>
+    <script src="popper.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <script src="holder.min.js"></script>
+    <script>
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function() {
+        'use strict';
+
+        window.addEventListener('load', function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
+    </script>
+
 </body>
 </html>
